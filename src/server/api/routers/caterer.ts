1d31e21de6e4b99bbb0817f155cb565519b59d26
Jeremy Jee <jeremy_jee@tech.gov.sg>
2025-07-05 20:03:27 +0800

    chore: small change

--- src/server/api/routers/caterer.ts
+++ src/server/api/routers/caterer.ts
@@ -2,6 +2,29 @@ import type { Prisma } from "@prisma/client";
 import z from "zod/v4";
 import { activeUserProcedure, createTRPCRouter } from "~/server/api/trpc";
 
+export const catererRouter = createTRPCRouter({
+	getCaterers: activeUserProcedure.query(async ({ ctx }) => {
+		return await ctx.db.caterer.findMany({
+			include: includeCatererListData,
+		});
+	}),
+	getCaterer: activeUserProcedure
+		.input(
+			z.object({
+				id: z.string(),
+			}),
+		)
+		.query(async ({ ctx, input }) => {
+			return await ctx.db.caterer.findUnique({
+				where: { id: input.id },
+				include: includeCatererData,
+			});
+		}),
+	getRestrictedAreas: activeUserProcedure.query(async ({ ctx }) => {
+		return await ctx.db.restrictedArea.findMany();
+	}),
+});
+
 export const includeCatererListData = {
 	menus: {
 		include: {
@@ -37,26 +60,3 @@ export const includeCatererData = {
 export type CatererData = Prisma.CatererGetPayload<{
 	include: typeof includeCatererData;
 }>;
-
-export const catererRouter = createTRPCRouter({
-	getCaterers: activeUserProcedure.query(async ({ ctx }) => {
-		return await ctx.db.caterer.findMany({
-			include: includeCatererListData,
-		});
-	}),
-	getCaterer: activeUserProcedure
-		.input(
-			z.object({
-				id: z.string(),
-			}),
-		)
-		.query(async ({ ctx, input }) => {
-			return await ctx.db.caterer.findUnique({
-				where: { id: input.id },
-				include: includeCatererData,
-			});
-		}),
-	getRestrictedAreas: activeUserProcedure.query(async ({ ctx }) => {
-		return await ctx.db.restrictedArea.findMany();
-	}),
-});
