c188cba273c2a1f3dc5b2ef0093abc5561cddde4
Adriel Seah <adriel_seah@hotmail.com>
2025-08-18 16:25:45 +0800

    updated prisma.json

--- prisma.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from '@prisma/internals';
-
-export default defineConfig({
-  seeds: {
-    default: 'npx tsx -r tsconfig-paths/register prisma/seed.ts',
-    prod: 'npx tsx -r tsconfig-paths/register prisma/seed-prod.ts',
-  },
-});
