dd544067df0f141bed66f1f72a6d2ea3b9414a13
Adriel <adriel_seah@hotmail.com>
2025-11-05 00:24:48 +0800

    updated banner

--- src/app/_components/ui/banner.tsx
+++ src/app/_components/ui/banner.tsx
@@ -16,17 +16,25 @@ export const Banner = () => {
 			<div className="px-3 lg:container lg:px-6">
 				<div className="flex items-center justify-between py-3">
 					<div className="flex items-center gap-3">
-						<div className="flex items-center gap-2">
+						<div className="flex items-start gap-2">
                         ⚠️ 
-							<div className="flex flex-wrap items-center gap-2">
-                                <strong>Disclaimer:</strong> This site is for price and menu comparison only. Information may not reflect the latest updates. Please check VITAL infosite at{" "}
-									<a
-										href="https://sgdcs.sgnet.gov.sg/sites/VITAL-pts/SitePages/CateringServices(2024-2027).aspx"
-										className="text-blue-600 underline hover:text-blue-800 font-semibold"
-									>
-										link
-									</a>{" "}
-                                    for the most current details.
+							<div>
+								<strong>Disclaimer:</strong>{" "}
+								This site provides price and menu comparison only and information may not reflect the latest updates. Please submit feedback through this{" "} 
+								<a
+									href="https://go.gov.sg/cateringfeedback"
+									className="text-blue-600 underline hover:text-blue-800 font-semibold"
+								>
+								form
+								</a>
+								{" "}and refer to{" "}
+								<a
+									href="https://sgdcs.sgnet.gov.sg/sites/VITAL-pts/SitePages/CateringServices(2024-2027).aspx"
+									className="text-blue-600 underline hover:text-blue-800 font-semibold"
+								>
+									VITAL infosite
+								</a>
+								{" "}for the most current details.
 							</div>
 						</div>
 					</div>
