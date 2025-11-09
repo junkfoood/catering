e7dace028e1f0d816d0e87749262ce79fe83f72c
Adriel <adriel_seah@hotmail.com>
2025-11-10 00:18:07 +0800

    updated with chatbot_2

--- src/env.ts
+++ src/env.ts
@@ -115,6 +115,6 @@ export const env = createEnv({
 	/**
 	 * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
 	 * useful for Docker builds.
-	 */
+	 **/
 	skipValidation: !!process.env.SKIP_ENV_VALIDATION,
 });
